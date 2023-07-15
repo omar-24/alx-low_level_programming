0f66cbfa4f46374fdbd18b3481c76f4b9ff64c14
omar-24 <alshweny2004@gmail.com>
2023-07-15 13:37:24 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
-#!/bin/bash
-#include<stdio.h>
 
-int main(){
+#include<stdio.h>
+/*
+ * main - point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void){
 	printif("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
